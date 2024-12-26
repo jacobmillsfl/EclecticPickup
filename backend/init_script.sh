6e496575c84f9ee74f2e020592e8b7eba5a65f1d
jacobmillsfl <link787@gmail.com>
2024-12-26 08:41:59 -0700

    Tech debt

--- backend/init_script.sh
+++ backend/init_script.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# Migrate database
-flask db init
+echo "Checking database connection..."
+flask db current || {
+    echo "Database not initialized. Setting migration baseline..."
+    flask db stamp head
+}
+
+echo "Applying any pending migrations..."
 flask db upgrade
 
 # Populate database
-python3 manage_db.py --database "/home/appuser/app/instance/database.db" --user "${ADMIN_USERNAME}" "${ADMIN_EMAIL}" "${ADMIN_PASSWORD}"
+#python3 manage_db.py --database "/home/appuser/app/instance/database.db" --user "${ADMIN_USERNAME}" "${ADMIN_EMAIL}" "${ADMIN_PASSWORD}"
 
 # Run app
+echo "Launching Flask App through uwsgi"
 /home/appuser/.local/bin/uwsgi --ini uwsgi.ini --socket 0.0.0.0:5000 --enable-threads --protocol=http -w wsgi:app
